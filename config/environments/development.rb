3f6975a6efac0549798db985f5c41faffd99f55e
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-01 03:05:38 +0000

    [Add]ActiveStrage,bootstrap,kaminariインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "33fa59a93cbe4d579942478a77cf063a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
