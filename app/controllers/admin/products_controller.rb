f492469a4298c4252b52ca08a1da8f12b906a5e6
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-02 21:37:09 +0000

    [Add]商品の登録画面、詳細画面、編集画面＆コントローラアクション&バリデーションの設定

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,3 +1,43 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_admin!
+  
+  def new
+    @product = Product.new
+  end 
+  
+  def index
+    @products = Product.all
+  end 
+  
+  def show
+    @product = Product.find(params[:id])
+  end 
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else 
+      render :new
+    end
+  end 
+  
+  def edit
+    @product = Product.find(params[:id])
+  end 
+  
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    else 
+      render :new
+    end 
+  end 
+  
+  private
+  def product_params
+    params.require(:product).permit(:image, :name, :explanation, :size, :price, :is_sold_out)
+  end 
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
+  
+  has_one_attached :image
   belongs_to :genre_id
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :size, presence: true
+  
+  def add_tax_price
+    (self.price * 1.10).round
+  end 
+  
 end
