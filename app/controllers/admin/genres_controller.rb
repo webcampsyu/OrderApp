8ee2599700ac22d2df4c4ce9cd129c35fef8b50f
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-04 01:26:45 +0000

    [Add]ジャンル一覧・追加、編集画面＆バリデーションの設定&コントローラアクションの作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,37 @@
 class Admin::GenresController < ApplicationController
+  
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end 
+  
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      @genres = Genre.all
+      render :index
+    else 
+      @genres = Genre.all
+      redirect_back(fallback_location: root_path)
+    end 
+  end 
+    
+  def edit
+    @genre = Genre.find(params[:id])
+  end 
+  
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update
+      redirect_to admin_genres_path
+    else
+      redirect_back(fallback_location: root_path)
+    end 
+  end 
+    
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end 
+  
 end
