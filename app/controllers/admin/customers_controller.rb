04f2015edfc027065c160e797b3aec6ae1f33466
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-09-04 01:37:46 +0000

    [Add]会員詳細画面&コントローラアクションの作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,4 +3,12 @@ class Admin::CustomersController < ApplicationController
   def index
     @customer = Customer.all
   end 
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end 
+  
+  def edit
+  end 
+  
 end
